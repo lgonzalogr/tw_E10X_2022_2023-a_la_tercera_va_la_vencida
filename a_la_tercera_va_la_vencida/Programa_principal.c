5659a1c4a49aa9548f3ce7e8fb62eb9056c592dc
Alejandro-Montero-Porras <alejandro.montero.porras@alumnos.upm.es>
2023-05-06 19:26:18 +0200

    Lectura completa del archivo
    Primera funcionalidad del programa hecha. El programa lee a través de una función el archivo completo. Se ha cambiado también el nombre del archivo para simplificar.
    
    Co-Authored-By: jesus marin delgado <79969611+jesuppedia@users.noreply.github.com>

--- a_la_tercera_va_la_vencida/Programa_principal.c
+++ a_la_tercera_va_la_vencida/Programa_principal.c
@@ -1,24 +1,47 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
+struct fila{
+    char filascompletas[432];
+};
 void menuprincipal(void);
-
+void mostrardatoscompletos(struct fila *, FILE *, int);
 int main(){
     int eleccion;
+    int i,j,n=0;
+    char c;
+    struct fila *nfilas; //Declaracion del puntero para reserva de memoria mas adelante
+    FILE *archivocompleto; //Declaracion del archivo principal
 
-    while(eleccion!=5){
+    while(eleccion!=6){
         menuprincipal();
         scanf("%i",&eleccion);
         switch(eleccion){
     case 1:
-        printf("\t1.\tVer datos completos.\n");
+        printf("\n\n\t1.\tVer datos completos.\n");
+        archivocompleto=fopen("generacion.csv","r");
+        while(!feof(archivocompleto)){ //Se cuenta el numero de filas
+        c=fgetc(archivocompleto);
+        if (c=='\n') n++;
+        }
+        printf("El numero de filas es: %i\n",n);
+        nfilas=(struct fila*)malloc(n*sizeof(struct fila)); //Se asigan dimension n=23 al vector de estructuras nfilas
+        rewind(archivocompleto);
+        mostrardatoscompletos(nfilas,archivocompleto,n);
+        fclose(archivocompleto);
+        //mostrardatoscompletos(nfilas,archivocompleto,n);
         break;
     case 2:
-        printf("\t2.\tBusqueda de datos.\n");
+        printf("Seleccione el tipo de busqueda que desea:\n");
         break;
     case 3:
-         printf("\t3.\tAñadir datos.\n");
+         printf("Seleccione los datos que desea añadir:\n");
         break;
+    case 4:
+        printf("Seleccione la operación estadistica que desea:\n");
+        break;
+    case 5:
+        printf("Indique como desea ver el historial:\n");
         }
 
 
@@ -38,5 +61,26 @@ void menuprincipal(void){
     printf("\t2.\tBusqueda de datos.\n");
     printf("\t3.\tAñadir datos.\n");
     printf("\t4.\tEstadísticas del archivo.\n");
-    printf("\t5.\tCerrar el programa.\n");
+    printf("\t5.\tHistorial.\n");
+    printf("\t6.\tCerrar el programa.\n");
+}
+void mostrardatoscompletos(struct fila *nfilas, FILE *archivocompleto,int n){
+    int i;
+
+    for(i=0;i<n;i++){
+
+	fgets(nfilas[i].filascompletas,sizeof(nfilas[i].filascompletas),archivocompleto);
+
+	nfilas[i].filascompletas[strlen(nfilas[i].filascompletas)-1]=';';
+
+	nfilas[i].filascompletas[strlen(nfilas[i].filascompletas)]='\0';
+
+}
+
+
+for(i=0;i<n;i++){
+
+	printf ("%s\n",nfilas[i].filascompletas);
+
+}
 }
